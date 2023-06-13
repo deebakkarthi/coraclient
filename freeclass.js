9dc2168eadb7c940ef8881ec380f82868eb48ca4
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-06-13 08:43:25 +0530

    Add: freeclass.html

---
+++ freeclass.js
@@ -0,0 +1,9 @@
+(async () => {
+  let response = await fetch("db/getAllSlot");
+  let slot = await response.json()
+  let slotEl = document.getElementById("slot");
+  for (var i = 0; i < slot.length; i++) {
+    let option = new Option(slot[i], slot[i]);
+    slotEl.appendChild(option);
+  }
+})();
