506078bf4b975f7fc2c58b72b2e545055c2bc5a7
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-06-13 07:17:59 +0530

    Add: dbk version

---
+++ booking.js
@@ -0,0 +1,50 @@
+function getDate() {
+  let date = new Date()
+  const offset = date.getTimezoneOffset()
+  date = new Date(date.getTime() - (offset * 60 * 1000))
+  date = date.toISOString().split("T")[0]
+  return date
+}
+
+(async () => {
+  /* Check if a user is logged in
+    * If not redirect them to login
+    */
+  let mail = sessionStorage.getItem("mail");
+  if (mail) {
+    let facultyEl = document.getElementById("faculty");
+    facultyEl.value = sessionStorage.getItem("mail");
+  } else {
+    window.location.replace("login");
+  }
+
+  let response = await fetch("db/getAllSlot");
+  let slot = await response.json()
+  let slotEl = document.getElementById("slot");
+  for (var i = 0; i < slot.length; i++) {
+    let option = new Option(slot[i], slot[i]);
+    slotEl.appendChild(option);
+  }
+
+  response = await fetch("db/getAllClass");
+  let classArr = await response.json()
+  let classEl = document.getElementById("class");
+  for (var i = 0; i < classArr.length; i++) {
+    let option = new Option(classArr[i], classArr[i]);
+    classEl.appendChild(option);
+  }
+
+  response = await fetch("db/getAllSubject");
+  let subject = await response.json()
+  let subjectEl = document.getElementById("subject");
+  for (var i = 0; i < subject.length; i++) {
+    let option = new Option(subject[i], subject[i]);
+    subjectEl.appendChild(option);
+  }
+  const date = getDate();
+  let dateEl = document.getElementById("date");
+  dateEl.min = date;
+
+  return;
+})();
+
---
+++ callback.js
@@ -0,0 +1,21 @@
+(async () => {
+  const baseURL = window.location.protocol + "//" + window.location.host;
+  const path = "/oauth/exchange"
+  const queryString = window.location.search;
+  const targetURL = baseURL + path + queryString;
+  const loginStatusDiv = document.getElementById("loginStatus");
+
+  let response = await fetch(targetURL);
+  let responseText = await response.text();
+  if (!response.ok) {
+    loginStatusDiv.innerHTML = "Login Failed\n" + responseText;
+    return;
+  } else {
+    let responseJSON = JSON.parse(responseText)
+    sessionStorage.setItem("mail", responseJSON["mail"]);
+    sessionStorage.setItem("name", responseJSON["name"]);
+    sessionStorage.setItem("organization", responseJSON["organization"]);
+    loginStatusDiv.textContent = `You are logged in as ${responseJSON["name"]}
+  [${responseJSON["mail"]}] from ${responseJSON["organization"]}`;
+  }
+})();
